e46bc108a7c703d4212cd9db23a971a6d148b210
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2017-11-29 19:26:23 +0100

    Klasa Statystyk poprawiona i mozliwa do kompilacji
    trzeba bylo dokonac kilku zmian

--- source/info/bockowsk/Statystyk.java
+++ source/info/bockowsk/Statystyk.java
@@ -13,12 +13,11 @@ class Statystyk <T extends Book & Serializable> {
         return t.tresc.split("\\W+");
     }
 
-    HashMap<String,LinkedList<T>> createWordStatistics(T t, String[] slowa) {
-        HashMap<String,LinkedList<T>> mapa1=new HashMap<String,LinkedList<T>>();
+    HashMap<String,LinkedList<String>> createWordStatistics(String[] slowa) {
+        HashMap<String,LinkedList<String>> mapa1=new HashMap<String,LinkedList<String>>();
         for (String s: slowa) {
             String litera=String.valueOf(s.charAt(0)).toLowerCase();
                 if (mapa1.containsKey(litera)) {
-//blad, bo LinkedList ma Ksiazka, a nie String
                     mapa1.get(litera).add(s);
                 }
                 else {
@@ -29,7 +28,7 @@ class Statystyk <T extends Book & Serializable> {
         return mapa1;
     }
 
-    ArrayList<String> wyswietlNajczestsze(HashMap<String,LinkedList<T>> mapa1) {
+    ArrayList<String> wyswietlNajczestsze(HashMap<String,LinkedList<String>> mapa1) {
         ArrayList<String> najczestsze=new ArrayList<String>();
         int maksymalna=0;
         for (String l: mapa1.keySet()) {
@@ -45,11 +44,33 @@ class Statystyk <T extends Book & Serializable> {
         }
         return najczestsze;
     }
+    
+    public static void main(String[] args) {
+        Ebook ksiazka1=new Ebook("Mark Lutz", "Wprowadzenie Python", "Helion");
+        Ebook ksiazka2=new Ebook("Stephen Kochan", "Programming C", "Sams Publishing");
+        Statystyk<Book> statystyk1=new Statystyk<Book>();
+        String[] slowa=statystyk1.getWordsInLyrics(ksiazka1);
+        HashMap<String,LinkedList<String>> statystyki=statystyk1.createWordStatistics(slowa);
+        ArrayList<String> najczestsze=statystyk1.wyswietlNajczestsze(statystyki);
+        String c1=String.format("KSIAZKA:\n\t%s\n\t%s",ksiazka1.autor,ksiazka1.tytul);
+        System.out.println(c1);
+        for (String i: najczestsze) {
+            System.out.println(statystyki.get(i));
+        }
+        Statystyk<Book> statystyk2=new Statystyk<Book>();
+        String[] slowa2=statystyk2.getWordsInLyrics(ksiazka2);
+        HashMap<String,LinkedList<String>> statystyki2=statystyk2.createWordStatistics(slowa2);
+        ArrayList<String> najczestsze2=statystyk2.wyswietlNajczestsze(statystyki2);
+        String c2=String.format("KSIAZKA:\n\t%s\n\t%s",ksiazka2.autor,ksiazka2.tytul);
+        System.out.println(c2);
+        for (String i: najczestsze2) {
+            System.out.println(statystyki2.get(i));
+        }
+
+    }
 
 /* TODO:
  * - stworzyc comparable dla Book zeby sie dalo sortowac (na podstawie liter, ktory ma najwiecej slow z wystapieniami)
  * - zaimplementowac tutaj zapisywanie obiektow
- * - stworzyc metody,ktore beda szukac jaka litera wystepuje najczesciej (generic methods) i ile razy
- * - metody do fajnego printowania (formatter)
  */
 }
