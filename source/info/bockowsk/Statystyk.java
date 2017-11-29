3a109d68c47428bab4bbce4ce7c3b0ec81d6dc50
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2017-11-29 23:15:17 +0100

    Przrobka statystyka aby podawal tylko litere
    podawana jest tylko litera dla ksiazki, kod znacznie skrocony, ale
    niestety nie ma nic po za tym

--- source/info/bockowsk/Statystyk.java
+++ source/info/bockowsk/Statystyk.java
@@ -9,11 +9,8 @@ import java.util.LinkedList;
 //
 class Statystyk <T extends Book & Serializable> {
 
-    String[] getWordsInLyrics(T t) {
-        return t.tresc.split("\\W+");
-    }
-
-    HashMap<String,LinkedList<String>> createWordStatistics(String[] slowa) {
+    static <U extends Book & Serializable> ArrayList<String> najczestsze(U t) {
+        String[] slowa=t.tresc.split("\\W+");
         HashMap<String,LinkedList<String>> mapa1=new HashMap<String,LinkedList<String>>();
         for (String s: slowa) {
             String litera=String.valueOf(s.charAt(0)).toLowerCase();
@@ -25,10 +22,6 @@ class Statystyk <T extends Book & Serializable> {
                     mapa1.get(litera).add(s);
                 }
         }   
-        return mapa1;
-    }
-
-    ArrayList<String> wyswietlNajczestsze(HashMap<String,LinkedList<String>> mapa1) {
         ArrayList<String> najczestsze=new ArrayList<String>();
         int maksymalna=0;
         for (String l: mapa1.keySet()) {
@@ -48,23 +41,14 @@ class Statystyk <T extends Book & Serializable> {
     public static void main(String[] args) {
         Ebook ksiazka1=new Ebook("Mark Lutz", "Wprowadzenie Python", "Helion");
         Ebook ksiazka2=new Ebook("Stephen Kochan", "Programming C", "Sams Publishing");
-        Statystyk<Book> statystyk1=new Statystyk<Book>();
-        String[] slowa=statystyk1.getWordsInLyrics(ksiazka1);
-        HashMap<String,LinkedList<String>> statystyki=statystyk1.createWordStatistics(slowa);
-        ArrayList<String> najczestsze=statystyk1.wyswietlNajczestsze(statystyki);
-        String c1=String.format("KSIAZKA:\n\t%s\n\t%s",ksiazka1.autor,ksiazka1.tytul);
-        System.out.println(c1);
-        for (String i: najczestsze) {
-            System.out.println(statystyki.get(i));
-        }
-        Statystyk<Book> statystyk2=new Statystyk<Book>();
-        String[] slowa2=statystyk2.getWordsInLyrics(ksiazka2);
-        HashMap<String,LinkedList<String>> statystyki2=statystyk2.createWordStatistics(slowa2);
-        ArrayList<String> najczestsze2=statystyk2.wyswietlNajczestsze(statystyki2);
-        String c2=String.format("KSIAZKA:\n\t%s\n\t%s",ksiazka2.autor,ksiazka2.tytul);
-        System.out.println(c2);
-        for (String i: najczestsze2) {
-            System.out.println(statystyki2.get(i));
+        
+        ArrayList<ArrayList<String>> najczestsze=new ArrayList<ArrayList<String>>();
+        najczestsze.add(najczestsze(ksiazka1));najczestsze.add(najczestsze(ksiazka2));
+        
+        for (ArrayList<String> a: najczestsze) {
+            for (String s: a) {
+                System.out.println(s);
+            }
         }
 
     }
